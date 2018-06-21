a76f3f9d8c8b98de9b0f5fc486ea769790826753
vaishiyer16 <vaishnavi.l.iyer@gmail.com>
2018-06-21 18:18:54 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -21,12 +21,14 @@ perfect_10_recipe = {
  # end
 
 # #2: Write code that returns the amount of chocolate chips from perfect_10 ingredients that is needed in the recipe.
-puts perfect_10_recipe["mini chocolate chips"]
-
+# puts perfect_10_recipe["mini chocolate chips"]
 
 # #3: Use `puts` to print out every ingredient and amount in the hash by iterating through the collection. You'll want to use the `each` method.
-# def ingredients_and_amounts(perfect_10_recipe)
-# end
+def ingredients_and_amounts(perfect_10_recipe)
+  perfect_10_recipe.each do |x,y|
+    puts "#{x}"
+    puts "#{y}"
+ end
 #
 # #4: Now use `puts` to print out just the ingredients without the amounts. You'll want to use the `each_key` method.
 # def ingredients(perfect_10_recipe)
