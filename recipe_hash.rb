1a94b48bcd25c6c855fd7ab984e99e4bf32b9000
vaishiyer16 <vaishnavi.l.iyer@gmail.com>
2018-06-21 18:08:31 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -13,12 +13,13 @@ perfect_10_recipe = {
   "olive oil" => "1/2 cup",
   "agave" => "1/4 cup"
 }
-puts perfect_10_recipe["key","values"]
+puts perfect_10_recipe
 
-# #2. Write a method that simply returns (not puts) your perfect_10 hash
-# def recipe_ingredients(perfect_10_recipe)
-# end
-#
+ #2. Write a method that simply returns (not puts) your perfect_10 hash
+ def recipe_ingredients(perfect_10_recipe)
+   
+ end
+
 # #2: Write code that returns the amount of chocolate chips from perfect_10 ingredients that is needed in the recipe.
 # def amount_of_chocolate_chips(perfect_10_recipe)
 # end
